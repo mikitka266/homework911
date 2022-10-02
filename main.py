3eb77d753570828bf05d64f71ea9150f4b03f387
Mikitka <mikitka266@gmail.com>
2022-10-02 19:00:41 +0300

    bot2

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-bot = Bot(token='')
-updater = Updater(token='')
+bot = Bot(token='5644828597:AAFsWAWd0odE-9lN9h_okaySHTGTJEe4L00')
+updater = Updater(token='5644828597:AAFsWAWd0odE-9lN9h_okaySHTGTJEe4L00')
 dispatcher = updater.dispatcher
 
 
 def message(update, context):
     text = update.message.text
-    context.bot.send_message(update.effective_chat.id, f'{text * 10}\n Я повторюшка :)')
+    context.bot.send_message(update.effective_chat.id, f'{text * 10}\t Я повторюшка :)')
 
 
 message_handler = MessageHandler(Filters.text, message)
